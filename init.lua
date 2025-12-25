158915d95dae68ceecc3d617e33ebf4eaa0fd4c2
Ryad <ryad.meftahi@gmail.com>
2025-12-24 22:00:17 -0500

    fix: pin nvim-treesitter to master branch
    Pin nvim-treesitter to the master branch to avoid compatibility issues
    with the new main branch API. The main branch has removed configs.lua
    and uses a completely rewritten API that kickstart.nvim hasn't migrated
    to yet.

--- init.lua
+++ init.lua
@@ -941,6 +941,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'master', -- Pin to master until main branch migration is complete
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
